180cd55fc9deffe38c8f5360a2792c22adfa139a
Marek Meyer <marek_meyer@outlook.com>
2018-04-15 15:23:31 +0100

    Implemented 10 fold cross validation

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,6 +6,8 @@ import csv
 import sys
 from nltk.corpus import stopwords
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import KFold
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -28,13 +30,41 @@ def create_classifier(features):
     print 'accuracy:', nltk.classify.util.accuracy(classifier, test_set)
 
 
-def create_features(records):
+def create_classifier_10_fold(features):
+    k_fold_validator = KFold(n_splits=10, shuffle=True)
 
+    accuracies = []
+
+    for train_index, test_index in k_fold_validator.split(features):
+        train_features = [features[i] for i in train_index]
+        test_features = [features[i] for i in test_index]
+
+        print'train on %d instances, test on %d instances' % (len(train_features), len(test_features))
+        classifier = NaiveBayesClassifier.train(train_features)
+
+        accuracy = nltk.classify.util.accuracy(classifier, test_features)
+        accuracies.append(accuracy)
+
+    print 'Accuracy 10 fold: ', sum(accuracies) / len(accuracies)
+
+
+def create_features(records):
+    # TODO Do this after after pre-processing
     filtered_records = filter_duplicates(records)
+
+    pos = 0
+    neg = 0
+    for record in filtered_records:
+        if record[0] == '1':
+            pos = pos + 1
+        else:
+            neg = neg + 1
+
+    print 'pos', pos
+    print 'neg', neg
 
     features = []
     for record in filtered_records:
-
         tokens = create_tokens(record[1])
         no_stopword_tokens = remove_stopwords(tokens)
 
@@ -43,10 +73,9 @@ def create_features(records):
         bigrams = nltk.ngrams(no_stopword_tokens, 2)
         bigram_features = {bigram: True for bigram in bigrams}
 
-        features.append([unigram_features, int(record[0])])
-        # features.append([merge_two_dicts(unigram_features, bigram_features), int(record[0])])
+        # features.append([unigram_features, int(record[0])])
+        features.append([merge_two_dicts(unigram_features, bigram_features), int(record[0])])
 
-    print features
     return features
 
 
@@ -68,21 +97,18 @@ def filter_duplicates(records):
 
 
 def merge_two_dicts(x, y):
-    z = x.copy()   # start with x's keys and values
-    z.update(y)    # modifies z with y's keys and values & returns None
+    z = x.copy()  # start with x's keys and values
+    z.update(y)  # modifies z with y's keys and values & returns None
     return z
+
+
+def print_features(features):
+    print (features)
+    with open('features.json', 'w') as file:
+        for feature in features:
+            file.write("%s\n" % feature)
 
 
 features = create_features(training_data)
 create_classifier(features)
-
-# iris = datasets.load_iris()
-# digits = datasets.load_digits()
-#
-# print(digits.images[0])
-#
-# classifier = svm.SVC(gamma=0.001, C=100)
-#
-# classifier.fit(digits.data[:-1], digits.target[:-1])
-#
-# print(classifier.predict(digits.data[-1:]))
+create_classifier_10_fold(features)

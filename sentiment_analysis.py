2e8e3b22d5da467d402275aa660e453df1d3b4b8
Marek Meyer <marek_meyer@outlook.com>
2018-04-18 13:17:53 +0100

    Implementation of multiple feature selection techniques

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,13 +1,14 @@
-from sklearn import datasets, svm
-import nltk
-from nltk.classify import NaiveBayesClassifier
-from nltk.tokenize import wordpunct_tokenize, word_tokenize
 import csv
 import sys
+
+import nltk
+from nltk.classify import NaiveBayesClassifier, util
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.model_selection import KFold
-import json
+from sklearn.svm import SVC
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -20,51 +21,47 @@ data_file = open('training.txt')
 training_data = list(csv.reader(data_file, delimiter='\t'))
 
 
-def create_classifier(features):
-    set_split = int(len(features) * 0.9)
-    training_set = features[:set_split]
-    test_set = features[set_split:]
+def create_and_evaluate_classifier_10_fold(features):
 
-    print'train on %d instances, test on %d instances' % (len(training_set), len(test_set))
-    classifier = NaiveBayesClassifier.train(training_set)
-    print 'accuracy:', nltk.classify.util.accuracy(classifier, test_set)
-
-
-def create_classifier_10_fold(features):
-    k_fold_validator = KFold(n_splits=10, shuffle=True)
-
+    k_fold_validator = KFold(n_splits=10, shuffle=False)
     accuracies = []
+    counter = 1
 
     for train_index, test_index in k_fold_validator.split(features):
-        train_features = [features[i] for i in train_index]
+        # print str(counter * 10) + '% cross-validation'
+
+        # Split features in training and test set
+        training_features = [features[i] for i in train_index]
         test_features = [features[i] for i in test_index]
 
-        print'train on %d instances, test on %d instances' % (len(train_features), len(test_features))
-        classifier = NaiveBayesClassifier.train(train_features)
+        # Create classifier
+        classifier = NaiveBayesClassifier.train(training_features)
 
-        accuracy = nltk.classify.util.accuracy(classifier, test_features)
+        # Evaluate classifier
+        accuracy = util.accuracy(classifier, test_features)
+
         accuracies.append(accuracy)
+        counter = counter + 1
 
-    print 'Accuracy 10 fold: ', sum(accuracies) / len(accuracies)
+    print 'Accuracy:', sum(accuracies) / float(len(accuracies)), '\n'
 
 
-def create_features(records):
-    # TODO Do this after after pre-processing
-    filtered_records = filter_duplicates(records)
-
-    pos = 0
-    neg = 0
-    for record in filtered_records:
-        if record[0] == '1':
-            pos = pos + 1
-        else:
-            neg = neg + 1
-
-    print 'pos', pos
-    print 'neg', neg
+def create_unigram_features(records):
 
     features = []
-    for record in filtered_records:
+    for record in records:
+        tokens = create_tokens(record[1])
+        no_stopword_tokens = remove_stopwords(tokens)
+
+        unigram_features = {token: True for token in no_stopword_tokens}
+        features.append([unigram_features, int(record[0])])
+    return features
+
+
+def create_uni_and_bigram_features(records):
+
+    features = []
+    for record in records:
         tokens = create_tokens(record[1])
         no_stopword_tokens = remove_stopwords(tokens)
 
@@ -73,21 +70,48 @@ def create_features(records):
         bigrams = nltk.ngrams(no_stopword_tokens, 2)
         bigram_features = {bigram: True for bigram in bigrams}
 
-        # features.append([unigram_features, int(record[0])])
         features.append([merge_two_dicts(unigram_features, bigram_features), int(record[0])])
+
+    return features
+
+
+def create_bigram_features(records):
+
+    features = []
+    for record in records:
+        tokens = create_tokens(record[1])
+        no_stopword_tokens = remove_stopwords(tokens)
+
+        bigrams = nltk.ngrams(no_stopword_tokens, 2)
+        bigram_features = {bigram: True for bigram in bigrams}
+        features.append([bigram_features, int(record[0])])
+
+    return features
+
+
+def create_pos_tag_features(records):
+
+    features = []
+    for record in records:
+        tokens = create_tokens(record[1])
+        post_tags = create_pos_tags(tokens)
+
+        unigram_features = {token: True for token in post_tags}
+        features.append([unigram_features, int(record[0])])
 
     return features
 
 
 # Creates tokens, including case conversion
 def create_tokens(text):
-    return wordpunct_tokenize(text.lower())
+    return word_tokenize(text.lower())
 
 
 # Remove english stopword tokens
 def remove_stopwords(tokens):
     stopword_set = set(stopwords.words('english'))
-    return [token for token in tokens if token not in stopword_set]
+    tokens = [token for token in tokens if token not in stopword_set]
+    return tokens
 
 
 # Filter duplicates in data set
@@ -96,19 +120,56 @@ def filter_duplicates(records):
     return [list(record) for record in set(tuples)]
 
 
-def merge_two_dicts(x, y):
-    z = x.copy()  # start with x's keys and values
-    z.update(y)  # modifies z with y's keys and values & returns None
-    return z
+def merge_two_dicts(dict1, dict2):
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
 
 
 def print_features(features):
     print (features)
-    with open('features.json', 'w') as file:
+    with open('features.json', 'w') as features_file:
         for feature in features:
-            file.write("%s\n" % feature)
+            features_file.write("%s\n" % feature)
 
 
-features = create_features(training_data)
-create_classifier(features)
-create_classifier_10_fold(features)
+def count_pos_and_neg_entries(records):
+    pos = 0
+    neg = 0
+    for record in records:
+        if record[0] == '1':
+            pos = pos + 1
+        else:
+            neg = neg + 1
+
+    print 'pos', pos
+    print 'neg', neg
+
+
+def create_pos_tags(tokens):
+    return nltk.pos_tag(tokens)
+
+
+def __main__():
+    filtered_records = filter_duplicates(training_data)
+    count_pos_and_neg_entries(filtered_records)
+
+    unigram_features = create_unigram_features(filtered_records)
+    uni_and_bigram_features = create_uni_and_bigram_features(filtered_records)
+    bigram_features = create_bigram_features(filtered_records)
+    pos_tag_features = create_pos_tag_features(filtered_records)
+
+    print 'Train and evaluate unigram features:'
+    create_and_evaluate_classifier_10_fold(unigram_features)
+
+    print 'Train and evaluate on unigram and bigram features:'
+    create_and_evaluate_classifier_10_fold(bigram_features)
+
+    print 'Train and evaluate on bigram features'
+    create_and_evaluate_classifier_10_fold(uni_and_bigram_features)
+
+    print 'Train and evaluate on pos tag features'
+    create_and_evaluate_classifier_10_fold(pos_tag_features)
+
+
+__main__()
